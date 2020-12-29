2921852abaf44dcaa1688fb98c592d4125c433ee
Louise <louisepeiffer@hotmail.com>
2020-12-29 09:07:51 +0100

    exos 1 et 2 faits mais prompt s'affiche pas

---
+++ public/js/main.js
@@ -0,0 +1,9 @@
+// ex 1
+let chiffre1 = 5
+let chiffre2 = 10
+console.log(chiffre1 + chiffre2);
+
+// ex 2
+let age = prompt ('Votre âge?')
+let prenom = prompt ('Votre prénom?')
+console.log(`${prénom} a ${age} cette année`);
