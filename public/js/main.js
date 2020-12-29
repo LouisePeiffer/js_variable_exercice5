95ba27659f2714d9dac8b0442bb00901e4727d96
Louise <louisepeiffer@hotmail.com>
2020-12-29 09:49:19 +0100

    corrigé

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,13 @@
 // ex 1
-let chiffre1 = 5
-let chiffre2 = 10
-console.log(chiffre1 + chiffre2);
+// let chiffre1 = 5
+// let chiffre2 = 10
+// console.log(`${chiffre1} + ${chiffre2} = ${chiffre1 + chiffre2}`);
+// console.log(`${chiffre1} - ${chiffre2} = ${chiffre1 - chiffre2}`);
+// console.log(`${chiffre1} / ${chiffre2} = ${chiffre1 / chiffre2}`);
+// console.log(`${chiffre1} * ${chiffre2} = ${chiffre1 * chiffre2}`);
 
 // ex 2
-// let age = prompt ('Votre âge?')
-// let prenom = prompt ('Votre prénom?')
-// console.log(`${prenom} a ${age} ans cette année`);
+let age = parseInt(prompt ('Votre âge?'))
+// le permet parseInt de convertir le string en number
+let prenom = prompt ('Votre prénom?')
+console.log(`${prenom} aura ${age+3} ans dans 3 ans`);
